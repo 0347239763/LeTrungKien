79024d7df37497b43eda19c5266340207af603e2
Lê Trung Kiên <72645758+0347239763@users.noreply.github.com>
2020-10-17 15:04:20 +0700

    Bài thực hành 1
    v.v.v.v

---
+++ BÀI THỰC HÀNH 1/bai2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bai2
+{
+    class Employee
+    {
+        private int id;
+        private string name;
+        private int YearOfBirth;
+        private double salaryLevel;
+        private double basicSalary;
+        public double setSalaryLevel
+        {
+            get
+            {
+                return salaryLevel;
+            }
+            set
+            {
+                salaryLevel = value;
+            }
+        }
+        public double setBasicSalary
+        {
+            get
+            {
+                return basicSalary;
+            }
+            set
+            {
+                basicSalary = value;
+            }
+        }
+        public int getId()
+        {
+            return id;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public int getYearOfBirth()
+        {
+            return YearOfBirth;
+        }
+        public double getIncome()
+        {
+            return salaryLevel * basicSalary;
+        }
+        public void display()
+        {
+            Console.WriteLine("Thong thin ve nguoi lao dong:");
+            Console.Write("Dinh danh", id, "Ten", name, "Nam sinh", YearOfBirth, "Luong co ban", basicSalary, "Thu nhap", getIncome());
+        }
+
+    }
+
+    class APP
+    {
+         static void Main(string[] args)
+         {
+             Employee e = new Employee();
+             e.display();
+             Console.ReadKey();
+         }
+    }   
+}
